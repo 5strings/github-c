567886d0cbc38794cf75e5751b6303b03fd71c94
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-13 10:06:47 +1100

    //Add operator 2

--- main.c
+++ main.c
@@ -15,12 +15,53 @@
 // Bitwise
 // Special
 
+
+// Part 2 -
+// +=  -+  *=   /+
+// ++ --
+
+// & - AND
+// | - OR
+// ^ - Exclusicve OR
+//  << - left
+//  >> - right
+
+// Special oerators //
+// , comma     int a, b;
+// sizeof   c = sizeof(a);
+// & * . ->  - Advanced operator
+
+
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     int a = 10, b = 30, c = 15;
     
+//    a += 10;
+//    a = a+10;
+//
+//    int a = 20;
+//
+//    a =+ 10;
+//    a = d +10;
+    
+    //++a --a pre increment & decrement
+    //a++ a--  post increment & decre. current value by 1
+    
+//    int a = 100,b;
+//    b = a++;
+//
+//    a = 101;
+//    b = 100;
+//
+//    b = ++a;
+//
+//    a = 101;
+//    b = 101;
+    
+
     int marks = 9;
     
     if(marks>5) {
@@ -45,8 +86,6 @@ int main(int argc, const char * argv[]) {
     
     // Conditional exmaple
     c = (a>b)?a:b;
-    
-    
     
     
     return 0;
